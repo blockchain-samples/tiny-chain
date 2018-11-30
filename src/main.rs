629c563146a6012c989048dfd983bfddef0fc06f
iveltondequeirozm <iveltondequeiroz@gmail.com>
2018-11-29 20:49:22 -0400

    creating chain

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,21 @@ mod blockchain;
 
 
 fn main() {
-    println!("tiny blockchain");
+    let mut miner_addr = String::new();
+    let mut difficulty = String::new();
+    let mut choice = String::new();
+
+    print!("input  a miner addres: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut miner_addr);
+    print!("difficulty: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut difficulty);
+    let diff = difficulty.trim().parse::<u32>().expect("we need an integer");
+    println!("generating genesis block!");
+    let mut chain = blockchain::Chain::new(miner_addr.trim().to_string(), diff);
+    
+
+
+    
 }
